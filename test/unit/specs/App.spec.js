c8b38fa9e22ab0e8e7008ffd2fa823d20eae2739
Mike Dvorscak <mike.dvorscak@gmail.com>
2018-01-20 14:41:46 -0600

    Add App spec

---
+++ test/unit/specs/App.spec.js
@@ -0,0 +1,14 @@
+import { mount } from '@vue/test-utils';
+import App from '@/App';
+import Game from '@/components/Game';
+
+describe('App.vue', () => {
+  it('should contain exactly one game', () => {
+    const wrapper = mount(App);
+    expect(wrapper.findAll(Game).length).to.equal(1);
+  });
+  it('render it\'s wrapper', () => {
+    const wrapper = mount(App);
+    expect(wrapper.contains('#app')).to.equal(true);
+  });
+});
