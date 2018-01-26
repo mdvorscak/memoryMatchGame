79484b26f730dbf64f9ec85e73d5e386149c08fb
Mike Dvorscak <mike.dvorscak@gmail.com>
2018-01-25 19:34:26 -0600

    Add documentation and simplify card changing code
    Extracted array modification to it's own function

--- src/components/TheGame.vue
+++ src/components/TheGame.vue
@@ -52,13 +52,20 @@ export default {
       cards: [],
       matchFound: false,
       flippingActive: false,
+      /** The currently flipped cards important information
+       *  id - id of card. Not unique since there are multiple cards with the same id
+       *  position - the position of the card on the board (in the array)
+      */
       currentlyFlipped: {
         id: -1,
         position: -1
       },
       currentMessage: startMessage,
+      /** clicking is locked when an incorrect match is found but
+       *    the cards have not yet been flipped back down */
       clickingLocked: false,
       matches: 0,
+      /** Game starts inactive and activates when the user first clicks a card */
       gameActive: false,
       gameOver: false
     };
@@ -72,33 +79,39 @@ export default {
     Timer
   },
   methods: {
+    /** @description Initializes the cards on the board */
     initBoard() {
-      // get the underlying card models
       this.cards = cardProvider(this.numberOfCards, this.numberOfMatches);
     },
+    /** @description flips a card up, triggered by card clicks
+     *  @param id {number} - The id of the card
+     *  @param position {number} - the position of the card on the board
+    */
     flipCard(id, position) {
       this.gameActive = true;
+      /** Critical section below  */
       if (this.clickingLocked) return;
-      this.cards[position].flipped = true;
-      this.$set(this.cards, position, this.cards[position]);
+      this.setCard(position, { flipped: true });
+
       if (this.currentlyFlipped.id === id) {
         this.correctMatch(position);
       } else if (this.flippingActive) {
         this.incorrectMatch(position);
       } else {
-        // First card flipped
+        /** First card flipped */
         this.currentlyFlipped = { id, position };
         this.flippingActive = true;
       }
     },
+    /** @description Sets the cards to matched and if the number of matches
+     *    is the same as the number of the number of cards, the game ends.
+     *  @see {flipCard} for use
+     */
     correctMatch(position) {
       this.currentMessage = 'You found a match!';
       // set both of the cards to matched
-      this.cards[position].matched = true;
-      this.$set(this.cards, position, this.cards[position]);
-      this.cards[this.currentlyFlipped.position].matched = true;
-      this.$set(this.cards, this.currentlyFlipped.position,
-        this.cards[this.currentlyFlipped.position]);
+      this.setCard(position, { matched: true });
+      this.setCard(this.currentlyFlipped.position, { matched: true });
       this.matchFound = true;
       this.resetCards();
       this.matches += 1;
@@ -106,24 +119,36 @@ export default {
         this.endGame();
       }
     },
+    /** @description Ends the game */
     endGame() {
       this.currentMessage = 'Congrats, you won!';
       this.gameOver = true;
       this.gameActive = false;
     },
+    /** @description Resets the cards (flipping them down) after a 1 second delay
+     *  @see {flipCard} for use
+     */
     async incorrectMatch(position) {
       this.currentMessage = 'Try again';
       this.matchFound = false;
+      /** critical section */
       this.clickingLocked = true;
       await delay(1000);
       // flip both cards back down
-      this.flipCardDown(position);
-      this.flipCardDown(this.currentlyFlipped.position);
+      this.setCard(position, { flipped: false });
+      this.setCard(this.currentlyFlipped.position, { flipped: false });
       this.resetCards();
       this.clickingLocked = false;
+      /** end of critical section */
     },
-    flipCardDown(position) {
-      this.cards[position].flipped = false;
+    /** @description Helper method to set properties on cards array
+     *  @param position {number} - the position of the card in the array
+     *  @param props {Object} - The properties to set on the card
+    */
+    setCard(position, props) {
+      Object.keys(props).forEach((prop) => {
+        this.cards[position][prop] = props[prop];
+      });
       this.$set(this.cards, position, this.cards[position]);
     },
     resetCards() {
@@ -133,6 +158,9 @@ export default {
         position: -1
       };
     },
+    /** @description - Resets the board back to the initial start state
+     *  @notes - Triggered by the reset/play again button in TheGameControls.vue
+    */
     resetBoard() {
       this.matchFound = false;
       this.currentMessage = startMessage;
